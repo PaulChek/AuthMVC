4a365f7c7074d32857b5149edc761cfa9d436369
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-07-13 21:17:53 +0700

    Auth via cookie

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace AuthMVC.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+        [Authorize]
+        [Route("[action]")]
+        public IActionResult Secret() {
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult Authenticate() {
+            var claims = new List<Claim> {
+               new Claim(ClaimTypes.Name, "Paul"),
+               new Claim(ClaimTypes.Email, "Paul@check.com")
+            };
+
+            var identity = new ClaimsIdentity(claims, "Grandma Word");
+
+            var userprincipal = new ClaimsPrincipal(new[] { identity });
+
+            HttpContext.SignInAsync(userprincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,21 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AuthMVC {
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-        }
+            services.AddAuthentication("CookieAuth").AddCookie("CookieAuth", cong => {
+                cong.Cookie.Name = "AuthMVC";
+                cong.LoginPath = "/Authenticate";
+            });
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services.AddControllersWithViews();
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
@@ -23,10 +20,12 @@ namespace AuthMVC {
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
